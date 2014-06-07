c3d7010501b3faef615f0f0180dc4980449e97a3
Philipp Nanz <philippn@gmail.com>
2014-06-07 15:09:47 +0200

    More tests

---
+++ src/test/java/org/yql4j/test/YqlQueryBuilderTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2014 Philipp Nanz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package org.yql4j.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.yql4j.ResultFormat;
+import org.yql4j.YqlQuery;
+import org.yql4j.YqlQueryBuilder;
+
+/**
+ * Tests for the {@link YqlQueryBuilder}.
+ */
+public class YqlQueryBuilderTest {
+
+	@Test
+	public void testQueryString() throws Exception {
+		YqlQueryBuilder builder = YqlQueryBuilder.fromQueryString("select * from geo.oceans");
+		YqlQuery query = builder.build();
+		assertEquals("select * from geo.oceans", query.getQueryString());
+	}
+
+	@Test
+	public void testQueryAlias() throws Exception {
+		YqlQueryBuilder builder = YqlQueryBuilder.fromQueryAlias("philippn", "ocean");
+		YqlQuery query = builder.build();
+		assertEquals("philippn", query.getAliasPrefix());
+		assertEquals("ocean", query.getAliasName());
+	}
+
+	@Test
+	public void testRepeatedBuild() throws Exception {
+		YqlQueryBuilder builder = YqlQueryBuilder.fromQueryAlias("philippn", "ocean");
+		builder.withVariable("name", "Arctic Ocean");
+		YqlQuery query1 = builder.build();
+		builder.withVariable("name", "Indian Ocean");
+		YqlQuery query2 = builder.build();
+		assertEquals("Arctic Ocean", query1.getVariableValue("name"));
+		assertEquals("Indian Ocean", query2.getVariableValue("name"));
+	}
+
+	@Test
+	public void testQueryWithConsumerkeyAndSecret() throws Exception {
+		YqlQueryBuilder builder = YqlQueryBuilder.fromQueryString("select * from geo.oceans where name=@name");
+		builder.withConsumerKeyAndSecret("abc", "xyz");
+		YqlQuery query = builder.build();
+		assertEquals("abc", query.getConsumerKey());
+		assertEquals("xyz", query.getConsumerSecret());
+	}
+
+	@Test
+	public void testQueryWithDiagnosticsEnabled() throws Exception {
+		YqlQueryBuilder builder = YqlQueryBuilder.fromQueryString("select * from geo.oceans where name=@name");
+		assertFalse(builder.build().isDiagnostics());
+		builder.withDiagnosticsEnabled();
+		assertTrue(builder.build().isDiagnostics());
+	}
+
+	@Test
+	public void testQueryWithFormat() throws Exception {
+		YqlQueryBuilder builder = YqlQueryBuilder.fromQueryString("select * from geo.oceans where name=@name");
+		assertNull(builder.build().getFormat());
+		builder.withFormat(ResultFormat.JSON);
+		assertEquals(ResultFormat.JSON, builder.build().getFormat());
+	}
+
+	@Test
+	public void testQueryWithVariable() throws Exception {
+		YqlQueryBuilder builder = YqlQueryBuilder.fromQueryString("select * from geo.oceans where name=@name");
+		builder.withVariable("name", "Arctic Ocean");
+		YqlQuery query = builder.build();
+		assertEquals("Arctic Ocean", query.getVariableValue("name"));
+	}
+
+}
